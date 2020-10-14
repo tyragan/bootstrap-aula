fcb514950bb9a054f6a246f165fb237c8e1ec1c8
silvio guilherme <multiplayrs394@gmail.com>
2020-10-14 14:50:40 -0300

    mais códigos e javascript básico implementado

---
+++ pagina.js
@@ -0,0 +1,12 @@
+function pergunta(texto){
+    let pergunta=confirm(texto)
+    if (pergunta == true){
+        return alert('esse é o logo do site');
+    }else{ 
+        return alert('ok!, vamos retornar')
+    }
+}
+function clickProduto(idDoProduto){
+    window.open(URL="https://www.twitch.tv/hakthar")
+   
+}
